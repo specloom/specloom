42ef48f642498a6eddf9763d8335cd28c9d732d1
Shigeki Yamanaka <sy@actit.jp>
2026-03-02 12:20:41 +0900

    no message

--- examples/solid-start/src/routes/(admin).tsx
+++
@@ -1,6 +0,0 @@
-import { RouteSectionProps } from "@solidjs/router";
-import { AdminLayout } from "~/components/AdminLayout";
-
-export default function AdminLayoutRoute(props: RouteSectionProps) {
-  return <AdminLayout>{props.children}</AdminLayout>;
-}
--- packages/solidjs/src/components/ui/utils.ts
+++
@@ -1,2 +0,0 @@
-export const cx = (...values: Array<string | false | null | undefined>) =>
-  values.filter(Boolean).join(" ");
